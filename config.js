9fa74774a8150fd04a08ba0c9354d8f2ebbe7b7f
Wilfried Djoum Nguemtchue <wilfried.djoum@institutsaintjean.org>
2024-04-13 05:48:20 +0100

    feat: initialisation du back et des methodes necessaires

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    db: {
+        // host: "172.20.10.5",
+        host: "localhost",
+        user: "root",
+        password: "will",
+        database: "users",
+        connectTimeout: 60000
+    },
+    listPerPage: 10,
+};
+
+module.exports = config
